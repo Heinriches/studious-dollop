6503d861dc211d26a642114f4161e0dfcc2c1f62
Heinriches <heydrich.institut@gmail.com>
2025-06-09 11:14:19 -0300

    Project 002: first commit

---
+++ S1P2/src/main/java/com/evilgenius/s1p2/S1P2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.evilgenius.s1p2;
+
+/**
+ *
+ * @author Fabio
+ */
+public class S1P2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
