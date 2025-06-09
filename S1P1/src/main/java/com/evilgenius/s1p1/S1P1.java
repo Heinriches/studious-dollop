325f7805f2c8e282824f1ff3baaa4a80e1a03439
Heinriches <heydrich.institut@gmail.com>
2025-06-09 10:51:39 -0300

    S1P1 final commit

---
+++ S1P1/src/main/java/com/evilgenius/s1p1/S1P1.java
@@ -0,0 +1,11 @@
+
+package com.evilgenius.s1p1;
+
+// create a class named S1P1.
+public class S1P1 {
+    // this is teh 'main' method.
+    public static void main(String[] args) {
+        // this prints whatever is below.
+        System.out.println("Whatever you type here.. will be repeated!");
+    }
+}
