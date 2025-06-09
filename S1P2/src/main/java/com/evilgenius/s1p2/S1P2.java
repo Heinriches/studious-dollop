420525e49ca1a12f183a46e1d5b3e87f33e3f4b9
Heinriches <heydrich.institut@gmail.com>
2025-06-09 12:19:24 -0300

    Project 002: fixed variables

--- S1P2/src/main/java/com/evilgenius/s1p2/S1P2.java
+++ S1P2/src/main/java/com/evilgenius/s1p2/S1P2.java
@@ -1,16 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+//  Project 002 -   Program the computer to verify the correct password. In this
+//                  two-player exercise, one player gives hints ewhile the other
+//                  player types-in password-guesses. The computer will let the 
+//                  player know when he/ she is correct.
 
 package com.evilgenius.s1p2;
 
-/**
- *
- * @author Fabio
- */
+import javax.swing.*;
+
 public class S1P2 {
 
     public static void main(String[] args) {
+        
+        String input = JOptionPane.showInputDialog("What's your name?");
+        
         System.out.println("Hello World!");
     }
 }
