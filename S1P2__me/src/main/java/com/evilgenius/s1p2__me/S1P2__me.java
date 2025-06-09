0af0e9f00dd8aa928e6a08424f2c55d1d72c2796
Heinriches <heydrich.institut@gmail.com>
2025-06-09 17:22:13 -0300

    Project_002: created new 'S1P2__me' project

---
+++ S1P2__me/src/main/java/com/evilgenius/s1p2__me/S1P2__me.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.evilgenius.s1p2__me;
+
+/**
+ *
+ * @author Fabio
+ */
+public class S1P2__me {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
