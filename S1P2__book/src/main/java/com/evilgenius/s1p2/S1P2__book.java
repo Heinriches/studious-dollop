111216109566a0c9796e0b7ec06c46be88525d84
Heinriches <heydrich.institut@gmail.com>
2025-06-10 11:08:23 -0300

    Project 002: final commit for S1P2__book

---
+++ S1P2__book/src/main/java/com/evilgenius/s1p2/S1P2__book.java
@@ -0,0 +1,37 @@
+//  Project 002 -   Program the computer to verify the correct password. In this
+//                  two-player exercise, one player gives hints ewhile the other
+//                  player types-in password-guesses. The computer will let the 
+//                  player know when he/ she is correct.
+
+package com.evilgenius.s1p2;
+
+// first, allow for input-getting:
+import javax.swing.*;
+
+// create a class named S1P2:
+public class S1P2__book {
+
+    // 'main' method:
+    public static void main(String[] args) {
+        
+        // variables:
+        
+        // this String will hold the user's input:
+        String input;
+        
+                
+        // input
+        // get input now:
+        input = JOptionPane.showInputDialog("Enter the secret-message: ");
+        
+        // Processing:
+        // test for correctness, "Evil Genius" is my secret-message:
+        if(input.equals("Evil Genius")){            
+            // user got it right, so tell him/ her:
+            JOptionPane.showMessageDialog(null, "YOU GOT THE SECRET-MESSAGE!");
+        }else{
+            // if user got it wrong, tell him/ her:
+            JOptionPane.showMessageDialog(null,  "WRONG! hahaha!");
+        }
+    }
+}
--- S1P2__me/src/main/java/com/evilgenius/s1p2__me/S1P2__me.java
+++ S1P2__me/src/main/java/com/evilgenius/s1p2__me/S1P2__me.java
@@ -1,16 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+//  Project 002 -   Program the computer to verify the correct password. In this
+//                  two-player exercise, one player gives hints ewhile the other
+//                  player types-in password-guesses. The computer will let the 
+//                  player know when he/ she is correct.
 
 package com.evilgenius.s1p2__me;
 
-/**
- *
- * @author Fabio
- */
+import javax.swing.*;
+
 public class S1P2__me {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+                // variables:
+        int secretPassword = 123456;
+        int input;
+
+        
+        // input
+        input = Integer.parseInt(JOptionPane.showInputDialog("What is your guess for the password?"));
+        
+        // Processing:
+        while(input != secretPassword){
+            if(input < secretPassword){
+                System.out.println("You guessed wrong (too low)!");
+            } else{
+                System.out.println("You guessed wrong (too high)!");
+            }
+            
+            input = Integer.parseInt(JOptionPane.showInputDialog("What is your new guess for the password?"));
+        }
+        
     }
 }
